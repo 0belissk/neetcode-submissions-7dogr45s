12dbc7ab5668c9ea0eeb4cc05a06c8b3c8338dcc
Paul Quinto <137469655+0belissk@users.noreply.github.com>
2026-08-14 18:35:45 -0500

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean isValid(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '{' || s.charAt(i) == '(' || s.charAt(i) == '[') {
+                stack.push(s.charAt(i));
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                if (stack.peek() == '(' ){
+                    if (s.charAt(i) == ')') {
+                        stack.pop();
+                    } else {
+                        return false;
+                    }
+                } else if (stack.peek() == '{' ){
+                    if (s.charAt(i) == '}') {
+                        stack.pop();
+                    } else {
+                        return false;
+                    }
+                    
+                } else if (stack.peek() == '[' ){
+                    if (s.charAt(i) == ']') {
+                        stack.pop();
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
